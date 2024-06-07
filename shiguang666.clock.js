b8bba04c8654852d7a522f382031883e4d70b48d
shiguang <an_shiguang@163.com>
2024-06-07 10:59:07 +0800

    update

--- shiguang666.clock.js
+++ shiguang666.clock.js
@@ -1,17 +1,22 @@
 var clock
 
 $(document).ready(function () {
-  var myDate = new Date()
-  myDate.setFullYear(myDate.getFullYear(), 5, 7)
+  var current_year = new Date().getFullYear();
+  var myDate = new Date("6, 7, "+current_year+" 09:00:00");
+
+  var month = 5, day = 7
+  
   if (new Date() > myDate) {
-    var year = new Date().getFullYear(), month = 6, day = 7
-    var futureDate = new Date(year + 1, month - 1, day, 9)
-    var diff = futureDate.getTime() / 1000 - new Date().getTime() / 1000
+    var year = current_year+1
   } else {
-    var year = new Date().getFullYear(), month = 6, day = 7
-    var futureDate = new Date(year, month - 1, day, 9)
-    var diff = futureDate.getTime() / 1000 - new Date().getTime() / 1000
+    var year = current_year
   }
+  
+  document.getElementById('te').innerHTML = year
+
+  var futureDate = new Date(year, month, day, 9)
+  var diff = futureDate.getTime() / 1000 - new Date().getTime() / 1000
+
   clock = $('.clock').FlipClock(diff, {
     clockFace: 'DailyCounter',
     countdown: true
